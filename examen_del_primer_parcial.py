9b2f7080af7eb98452c9805ac049420079b714ca
KarenCasLop <144073992+KarenCasLop@users.noreply.github.com>
2025-02-26 19:53:19 -0600

    Update examen_del_primer_parcial.py

--- examen_del_primer_parcial.py
+++ examen_del_primer_parcial.py
@@ -108,22 +108,10 @@ def correlacion():
     x = np.array([100,120,140,160,180,200,220,240,260,280])
     y = np.array([1305710,1658277,1894167,2136552,2298267,2553624,2780503,3289726,3472743,3779477])
 
-    mean_x = np.mean(x)
-    mean_y = np.mean(y)
+    correlation_matrix = np.corrcoef(x, y)
+    correlation_coefficient = correlation_matrix[0, 1]
 
-    deviation_x = x - mean_x
-    deviation_y = y - mean_y
-
-    product_deviations = deviation_x * deviation_y
-
-    sum_product_deviations = np.sum(product_deviations)
-
-    std_x = np.std(x, ddof=1)
-    std_y = np.std(y, ddof=1)
-
-    r = sum_product_deviations / ((len(x) - 1) * std_x * std_y)
-
-    return r
+    return correlation_coefficient
 
 print(f'El coeficiente de Pearson es: {correlacion()}')
 
