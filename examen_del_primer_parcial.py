6b4d32ff9b0c31f7fa06e13c9f30dfc1d4c47aae
josej9 <159069337+josej9@users.noreply.github.com>
2025-02-26 19:45:16 -0600

    Update examen_del_primer_parcial.py

--- examen_del_primer_parcial.py
+++ examen_del_primer_parcial.py
@@ -104,7 +104,7 @@ Regresa el coeficiente de Pearson
 def correlacion():
     df_tamaño = np.array([100, 120, 140, 160, 180, 200, 220, 240, 260, 280])
     df_precio = np.array([1305710, 1658277, 1894167, 2136552, 2298267, 2553624, 2780503, 	3289726, 3472743, 3779477])
-    correlacion = np.corrcoef(df_tamaño, df_precio)
+    correlacion = np.corrcoef(df_tamaño, df_precio)[0,1]
     return correlacion
 
 correlacion()
